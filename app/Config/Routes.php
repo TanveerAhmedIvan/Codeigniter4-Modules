a8c82881f89c8d962179c6aa113ab58fced04632
Tanveer Ahmed Ivan <tanveerahmedivan@gmail.com>
2024-03-03 18:46:53 +0600

    routes updated

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Modules\Dashboard\Controllers\Home::index');
+$routes->get('/', '\Modules\Home\Controllers\Home::index');
 $routes->group('dashboard', ['namespace' => 'Modules\Dashboard\Controllers'], function($routes)
 {
     $routes->get('/', 'Dashboard::index');
